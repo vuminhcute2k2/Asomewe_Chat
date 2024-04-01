9ee176e361d86db2a3ad69f9620f286a30bea9cd
vuminhcute2k2 <minhvu69696969@gmail.com>
2024-04-02 03:31:07 +0700

    add danh sách bạn bè lên firebase khi ấn nút đồng ý

--- awesome_chat/lib/app/modules/home/tapbarfriends/controller/requestfriends_controller.dart
+++ awesome_chat/lib/app/modules/home/tapbarfriends/controller/requestfriends_controller.dart
@@ -1,15 +1,15 @@
-
-
-import 'package:get/get.dart';
+import 'package:awesome_chat/model/request_friends.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
+import 'package:get/get.dart';
 
 class RequestFriendsController extends GetxController {
   final FirebaseAuth _auth = FirebaseAuth.instance;
-  final _database = FirebaseDatabase.instance;
+  final DatabaseReference _database = FirebaseDatabase.instance.reference();
 
   String? currentUserID;
   RxList<Map<String, dynamic>> incomingRequests = <Map<String, dynamic>>[].obs;
+  RxList<Map<String, dynamic>> cancelRequests = <Map<String, dynamic>>[].obs;
 
   @override
   void onInit() {
@@ -18,6 +18,7 @@ class RequestFriendsController extends GetxController {
       if (user != null) {
         currentUserID = user.uid;
         fetchIncomingRequests();
+        fetchCancelRequest();
       } else {
         currentUserID = null;
         incomingRequests.clear();
@@ -30,7 +31,6 @@ class RequestFriendsController extends GetxController {
       return;
     }
     _database
-        .ref()
         .child('request_friends')
         .orderByChild('receiverId')
         .equalTo(currentUserID)
@@ -38,14 +38,20 @@ class RequestFriendsController extends GetxController {
         .listen((event) {
       final dataSnapshot = event.snapshot;
       if (dataSnapshot != null && dataSnapshot.value != null) {
-        final Map<dynamic, dynamic>? data = dataSnapshot.value as Map<dynamic, dynamic>?; 
+        final Map<dynamic, dynamic>? data =
+            dataSnapshot.value as Map<dynamic, dynamic>?;
         if (data != null) {
           List<Map<String, dynamic>> requests = [];
           data.forEach((key, value) {
             if (value['receiverId'] == currentUserID) {
               requests.add({
+                'key': key,
+                'senderId': value['senderId'],
                 'senderName': value['senderName'],
                 'senderPhotoUrl': value['senderPhotoUrl'],
+                'receiverName': value['receiverName'],
+                'receiverPhotoUrl': value['receiverPhotoUrl'],
+                'receiverId': value['receiverId']
               });
             }
           });
@@ -54,4 +60,85 @@ class RequestFriendsController extends GetxController {
       }
     });
   }
+
+  void fetchCancelRequest() {
+    if (currentUserID == null) {
+      return;
+    }
+    _database
+        .child('request_friends')
+        .orderByChild('senderId')
+        .equalTo(currentUserID)
+        .onValue
+        .listen((event) {
+      final dataSnapshot = event.snapshot;
+      if (dataSnapshot != null && dataSnapshot.value != null) {
+        final Map<dynamic, dynamic>? data =
+            dataSnapshot.value as Map<dynamic, dynamic>?;
+        if (data != null) {
+          List<Map<String, dynamic>> requestsCancel = [];
+          data.forEach((key, value) {
+            if (value['senderId'] == currentUserID) {
+              requestsCancel.add({
+                'receiverName': value['receiverName'],
+                'receiverPhotoUrl': value['receiverPhotoUrl'],
+              });
+            }
+          });
+          cancelRequests.value = requestsCancel;
+        }
+      }
+    });
+  }
+
+Future<void> acceptFriendRequest(FriendRequest friendRequest, String currentUserID) async {
+  
+  // Thêm thông tin của người đồng ý kết bạn vào nút friends của người gửi yêu cầu
+  await addFriend(currentUserID, {
+    'userId': friendRequest.senderId,
+    'userName': friendRequest.senderName,
+    'userPhotoUrl': friendRequest.senderPhotoUrl,
+  });
+
+  // Thêm thông tin của người gửi yêu cầu vào nút friends của người đồng ý kết bạn
+  await addFriend(friendRequest.senderId, {
+  'userId': currentUserID,
+  'userName': friendRequest.receiverName,
+  'userPhotoUrl': friendRequest.receiverPhotoUrl,
+});
+
+  // Xóa dữ liệu của id trùng với receiverId trong nút request_friends
+  await _database
+      .child('request_friends')
+      .orderByChild('receiverId')
+      .equalTo(currentUserID)
+      .once()
+      .then((snapshot) {
+    if (snapshot.snapshot.value != null) {
+      Map<dynamic, dynamic>? data = snapshot.snapshot.value as Map<dynamic, dynamic>?;
+      data?.forEach((key, value) {
+        if (value['receiverId'] == currentUserID) {
+          _database.child('request_friends').child(key).remove();
+        }
+      });
+    }
+  });
+  print('receiverName: ${friendRequest.receiverName}');
+  print('receiverPhotoUrl: ${friendRequest.receiverPhotoUrl}');
+  print('senderName: ${friendRequest.senderName}');
+  print('senderPhotoUrl: ${friendRequest.senderPhotoUrl}');
+  
+  
+}
+
+Future<void> addFriend(String userId, Map<String, dynamic> friendInfo) async {
+    await _database
+        .child('friends')
+        .child(userId)
+        .child(friendInfo['userId'])
+        .set(friendInfo);
+        
+  }
+
+
 }
--- awesome_chat/lib/app/modules/home/tapbarfriends/views/requestfriends_screen.dart
+++ awesome_chat/lib/app/modules/home/tapbarfriends/views/requestfriends_screen.dart
@@ -1,53 +1,154 @@
+import 'package:awesome_chat/model/request_friends.dart';
+import 'package:awesome_chat/service/store_service.dart';
 import 'package:awesome_chat/themes/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:awesome_chat/app/modules/home/tapbarfriends/controller/requestfriends_controller.dart';
 
 class RequestItem extends StatelessWidget {
-  final RequestFriendsController requestFriendsController = Get.put(RequestFriendsController());
+  final RequestFriendsController requestFriendsController =
+      Get.put(RequestFriendsController());
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       body: Obx(() {
-        if (requestFriendsController.incomingRequests.isEmpty) {
+        if (requestFriendsController.incomingRequests.isEmpty &&
+            requestFriendsController.cancelRequests.isEmpty) {
           return const Center(
             child: Text('No friend requests'),
           );
         } else {
           return SingleChildScrollView(
             child: Column(
-              
               children: [
                 const Padding(
                   padding: EdgeInsets.all(20.0),
                   child: Align(
-                    alignment: Alignment.centerLeft,
-                    child: Text('LỜI MỜI KẾT BẠN',style: TextStyle(color: AppColors.colorGrayText,fontSize: 16,fontWeight: FontWeight.w600),)),
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        'LỜI MỜI KẾT BẠN',
+                        style: TextStyle(
+                            color: AppColors.colorGrayText,
+                            fontSize: 16,
+                            fontWeight: FontWeight.w600),
+                      )),
                 ),
                 SizedBox(
                   width: double.infinity,
-                  height: 300,
+                  height: 150,
                   child: ListView.builder(
                     itemCount: requestFriendsController.incomingRequests.length,
                     itemBuilder: (context, index) {
                       final request = requestFriendsController.incomingRequests[index];
                       final senderName = request['senderName'] ?? '';
                       final senderPhotoUrl = request['senderPhotoUrl'] ?? '';
-                  
+
                       return ListTile(
                         leading: CircleAvatar(
                           backgroundImage: NetworkImage(senderPhotoUrl),
                         ),
                         title: Text(
                           senderName,
-                          style:const TextStyle(
+                          style: const TextStyle(
                             fontWeight: FontWeight.bold,
                           ),
                         ),
-                        onTap: () {
-                          // Xử lý khi người dùng nhấn vào một lời mời kết bạn
-                        },
+                        trailing: GestureDetector(
+                          onTap: () async {
+                            await requestFriendsController.acceptFriendRequest(
+                              FriendRequest(
+                                senderId: request['senderId'] ?? '',
+                                receiverId: request['receiverId'] ?? '',
+                                senderName: request['senderName'] ?? '',
+                                receiverName: request['receiverName'] ?? '',
+                                senderPhotoUrl: request['senderPhotoUrl'] ?? '',
+                                receiverPhotoUrl: request['receiverPhotoUrl'] ?? '',
+                              ),
+                              requestFriendsController.currentUserID!,
+                            
+                            );
+                          },
+                          child: Container(
+                            width: 73,
+                            height: 30,
+                            decoration: const BoxDecoration(
+                              color: AppColors.colorVioletText,
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(18)),
+                            ),
+                            child: const Center(
+                                child: Text(
+                              'Đồng ý ',
+                              style: TextStyle(
+                                color: Colors.white,
+                              ),
+                            )),
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+                Container(
+                  width: double.infinity,
+                  height: 5,
+                  color: Color(0XFFEFEEEE),
+                ),
+                const Padding(
+                  padding: EdgeInsets.all(20.0),
+                  child: Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        'ĐÃ GỬI KẾT BẠN',
+                        style: TextStyle(
+                            color: AppColors.colorGrayText,
+                            fontSize: 16,
+                            fontWeight: FontWeight.w600),
+                      )),
+                ),
+                SizedBox(
+                  width: double.infinity,
+                  height: 150,
+                  child: ListView.builder(
+                    itemCount: requestFriendsController.cancelRequests.length,
+                    itemBuilder: (context, index) {
+                      final requestCancel =
+                          requestFriendsController.cancelRequests[index];
+                      final receiverName = requestCancel['receiverName'] ?? '';
+                      final receiverPhotoUrl =
+                          requestCancel['receiverPhotoUrl'] ?? '';
+
+                      return ListTile(
+                        leading: CircleAvatar(
+                          backgroundImage: NetworkImage(receiverPhotoUrl),
+                        ),
+                        title: Text(
+                          receiverName,
+                          style: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        trailing: GestureDetector(
+                          onTap: () async {},
+                          child: Container(
+                            width: 73,
+                            height: 30,
+                            decoration: const BoxDecoration(
+                              color: AppColors.colorVioletText,
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(18)),
+                            ),
+                            child: const Center(
+                                child: Text(
+                              'Hủy ',
+                              style: TextStyle(
+                                color: Colors.white,
+                              ),
+                            )),
+                          ),
+                        ),
                       );
                     },
                   ),
